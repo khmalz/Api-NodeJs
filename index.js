8be5dbcae9a82b9afd69c12ba8dab22114c45735
khmalz <akmalrma1710@gmail.com>
2023-03-07 21:07:02 +0700

    Adding new module for check empty

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const isEmpty = require("is-empty");
 const response = require("./response");
 const db = require("./connection");
 
