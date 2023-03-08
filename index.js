6a41e87d67552f16495eb7571d3b0108cf4f659c
khmalz <akmalrma1710@gmail.com>
2023-03-08 15:30:47 +0700

    Wrap the code to src folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-const { port } = require("./utils/config");
+const app = require("./src/app");
+const { port } = require("./src/utils/config");
 
 app.listen(port, () => {
    console.log(`listening on http://localhost:${port}`);
