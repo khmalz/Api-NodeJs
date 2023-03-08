dd60ab6c3884f8e03ffa0033cced4aa131fef734
khmalz <akmalrma1710@gmail.com>
2023-03-08 10:52:38 +0700

    Modify index to clean & simple code

--- index.js
+++ index.js
@@ -13,70 +13,69 @@ app.get("/", (req, res) => {
    response(200, "Success", "API Node Js", res);
 });
 
-app.get("/pelajar", (req, res) => {
-   const sql = "SELECT * FROM pelajar";
-   db.query(sql, (err, fields) => {
-      if (err) response(500, "Invalid", err.sqlMessage, res);
+app.route("/pelajar")
+   .get((req, res) => {
+      const sql = "SELECT * FROM pelajar";
+      db.query(sql, (err, fields) => {
+         if (err) response(500, "Invalid", err.sqlMessage, res);
 
-      response(200, "Success", fields, res);
-   });
-});
-
-app.get("/pelajar/:np", (req, res) => {
-   const sql = `SELECT * FROM pelajar WHERE np = ${req.params.np}`;
-   db.query(sql, (err, fields) => {
-      if (err) throw err;
-
-      if (isEmpty(fields)) {
-         response(404, "Invalid", "Data tidak ditemukan", res);
-      } else {
          response(200, "Success", fields, res);
-      }
+      });
+   })
+   .post((req, res) => {
+      const { np, nama, alamat } = req.body;
+      const sql = `INSERT INTO pelajar (np, nama, alamat) VALUES (${np}, '${nama}', '${alamat}')`;
+
+      db.query(sql, (err, fields) => {
+         if (err) response(500, "Invalid", err.sqlMessage, res);
+
+         if (fields?.affectedRows) {
+            response(201, "Success", `Success created new pelajar with np ${np}`, res);
+         }
+      });
+   })
+   .put((req, res) => {
+      const { np, nama, alamat } = req.body;
+      const sql = `UPDATE pelajar SET np = ${np}, nama = '${nama}', alamat = '${alamat}' WHERE np = ${np}`;
+
+      db.query(sql, (err, fields) => {
+         if (err) response(500, "Invalid", err.sqlMessage, res);
+
+         if (fields?.affectedRows) {
+            response(200, "Success", `Success edited pelajar with np ${np}`, res);
+         } else {
+            response(404, "Invalid", "Data tidak ditemukan", res);
+         }
+      });
    });
-});
 
-app.post("/pelajar", (req, res) => {
-   const { np, nama, alamat } = req.body;
-   const sql = `INSERT INTO pelajar (np, nama, alamat) VALUES (${np}, '${nama}', '${alamat}')`;
+app.route("/pelajar/:np")
+   .get((req, res) => {
+      const sql = `SELECT * FROM pelajar WHERE np = ${req.params.np}`;
+      db.query(sql, (err, fields) => {
+         if (err) throw err;
 
-   db.query(sql, (err, fields) => {
-      if (err) response(500, "Invalid", err.sqlMessage, res);
+         if (isEmpty(fields)) {
+            response(404, "Invalid", "Data tidak ditemukan", res);
+         } else {
+            response(200, "Success", fields, res);
+         }
+      });
+   })
+   .delete((req, res) => {
+      const { np } = req.params;
+      const sql = `DELETE FROM pelajar WHERE np = ${np}`;
 
-      if (fields?.affectedRows) {
-         response(201, "Success", `Success created new pelajar with np ${np}`, res);
-      }
+      db.query(sql, (err, fields) => {
+         if (err) response(500, "Invalid", err.sqlMessage, res);
+
+         if (fields?.affectedRows) {
+            response(200, "Success", `Success deleted pelajar with np ${np}`, res);
+         } else {
+            response(404, "Invalid", "Data tidak ditemukan", res);
+         }
+      });
    });
-});
-
-app.put("/pelajar", (req, res) => {
-   const { np, nama, alamat } = req.body;
-   const sql = `UPDATE pelajar SET np = ${np}, nama = '${nama}', alamat = '${alamat}' WHERE np = ${np}`;
-
-   db.query(sql, (err, fields) => {
-      if (err) response(500, "Invalid", err.sqlMessage, res);
-
-      if (fields?.affectedRows) {
-         response(200, "Success", `Success edited pelajar with np ${np}`, res);
-      } else {
-         response(404, "Invalid", "Data tidak ditemukan", res);
-      }
-   });
-});
-
-app.delete("/pelajar/:np", (req, res) => {
-   const { np } = req.params;
-   const sql = `DELETE FROM pelajar WHERE np = ${np}`;
-
-   db.query(sql, (err, fields) => {
-      if (err) response(500, "Invalid", err.sqlMessage, res);
-
-      if (fields?.affectedRows) {
-         response(200, "Success", `Success deleted pelajar with np ${np}`, res);
-      } else {
-         response(404, "Invalid", "Data tidak ditemukan", res);
-      }
-   });
-});
 
 app.use((req, res) => {
    res.sendStatus(404);
